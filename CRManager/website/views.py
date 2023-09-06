2aebb4cdb5deae46d7837b819df206ef3d5f4886
RjCoder <ej84@njit.edu>
2023-09-06 15:20:44 -0400

    Second Update.

--- CRManager/website/views.py
+++ CRManager/website/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     # Login Check
     if request.method == 'POST':
         username = request.POST['username']
@@ -18,12 +20,14 @@ def home(request):
             messages.success(request, 'Failed to Log in. Please Try again!')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 def login_user(request):
+    # Can be used to create separate login page, form, or anything like that.
     pass
 
 def logout_user(request):
+    # Log out and redirects to the home page.
     logout(request)
     messages.success(request, 'You have signed out. See you later!')
     return redirect('home')
@@ -47,3 +51,25 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
     
     return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up Records
+        customer_record = Record.objects.get(id=pk) # use .get() to get single object.
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view that page!")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id=pk)
+        record.delete()
+        messages.success(request, 'Record deleted.')
+        return redirect('home')
+    else:
+        messages.success(request, 'You are not allowed to perform this action. Please sign in first!')
+        return redirect('home')
+    
+def new_record(request):
+    return render(request, 'new_record.html', {})
